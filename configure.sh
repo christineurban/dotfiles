christineurban/dotfiles

commit 07d79022a8bdd37bb086a1cc34b74db298ba8351

Add git_key helper function

configure.sh
@@ -24,8 +24,8 @@ configure_git() {
         exit
     fi
 
-    eval `ssh-agent -s`
-    ssh-add ~/.ssh/id_github
+    # Defined in .bashrc
+    git_key
 }
 
 configure_npm_packages() {

.bashrc
@@ -44,6 +44,11 @@ is_ubuntu() {
     return 1
 }
 
+git_key() {
+    eval `ssh-agent -s`
+    ssh-add -k ~/.ssh/id_github
+}
+
 if is_mac; then
     # General auto-complete
     if [ -f `brew --prefix`/etc/bash_completion ]; then

commit cba06eb71f9e805b4bc9bf4b626ff46bcefe5ff2

Fix bug introduced in 07d7902

configure.sh
@@ -24,8 +24,8 @@ configure_git() {
         exit
     fi
 
-    # Defined in .bashrc
-    git_key
+    eval `ssh-agent -s`
+    ssh-add -k ~/.ssh/id_github
 }
 
 configure_npm_packages() {